a24c888738250b5eeae646b4bc45680e97fa4fbe
zeldris457 <117319189+zeldris457@users.noreply.github.com>
2022-11-28 17:39:20 -0500

    Add files via upload

---
+++ clobundance.js
@@ -0,0 +1,10 @@
+const forms= document.querySelector(".forms"),
+      links = document.querySelectorAll(".link");
+
+links.forEach(link => {
+    link.addEventListener("click", e => {
+            e.preventDefault();
+            forms.classList.toggle("show-signup");
+
+        })
+})
